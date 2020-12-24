2c7d03887e2ed43ea4febc7cb7b56e4cde51fb38
Senthilkannan <51955692+techkanna@users.noreply.github.com>
2020-12-24 10:36:37 +0530

    completed basic version

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,34 @@
 import express from 'express'
+import Stripe from 'stripe'
+import asyncHandler from 'express-async-handler'
 
 const app = express()
 
 app.use(express.json())
+
+const stripe = new Stripe('sk_test_51I18f9FYQ9ouv5EjrtEHHc4A1wK3kJVttoWAcWhsR48wL4YDZYQ9VaNInDBUXjTtfqqefkujp3lrP3e8uoZ9890m0081XxK2Gb')
+
+app.post('/api/charge', asyncHandler(async (req, res) => {
+
+  const { id, amount } = req.body;
+
+  try {
+    const payment = await stripe.paymentIntents.create({
+      amount,
+      currency: 'INR',
+      description: 'test meyment',
+      payment_method: id,
+      confirm: true
+    })
+
+    return res.status(200).json({
+      confirm: 'test-abc',
+      payment
+    })
+  } catch (error) {
+    console.log(error);
+  }
+}))
 
 app.use('/', (req, res) => {
   res.send('api running...')
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,61 @@
 import React from 'react'
+import { loadStripe } from '@stripe/stripe-js'
+import {
+  CardElement,
+  Elements,
+  useStripe,
+  useElements,
+} from '@stripe/react-stripe-js'
+import axios from 'axios'
+
+const CheckoutForm = () => {
+  const stripe = useStripe()
+  const elements = useElements()
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: 'card',
+      card: elements.getElement(CardElement),
+    })
+
+    if (!error) {
+      const { id } = paymentMethod
+
+      console.log('id: ', id);
+      try {
+        const { data } = await axios.post('/api/charge', { id, amount: 100 })
+
+        console.log('res data: ', data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  }
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      style={{ width: '20rem', margin: '4rem auto' }}
+    >
+      <CardElement />
+      <button type="submit" disabled={!stripe}>
+        pay
+      </button>
+    </form>
+  )
+}
+
+const stripePromise = loadStripe(
+  'pk_test_51I18f9FYQ9ouv5EjYe1GUwJ50OoEohfN6qev6gHDWOlE75v8DVf9utk7BVp1Efv0OarbunNbtnTbMKxINPKODGDe00IYQ0wTuY'
+)
 
 function App() {
   return (
-    <>
-      <h1>Hallo senthil</h1>
-    </>
-  );
+    <Elements stripe={stripePromise}>
+      <CheckoutForm />
+    </Elements>
+  )
 }
 
-export default App;
+export default App
